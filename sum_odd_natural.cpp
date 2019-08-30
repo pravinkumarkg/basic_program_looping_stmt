311ec6d768d2bf8cfb9bddb03efbebbd123501bb
K G Pravin Kumar <35860045+pravinkumarkg@users.noreply.github.com>
2019-08-30 18:01:38 +0530

    Create sum_odd_natural.cpp

---
+++ sum_odd_natural.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int start,end,sum=0;
+	cin>>start>>end;
+	for(start=1;start<=end;start++)
+	{
+		if(start%2!=0)
+		{
+		sum+=start;
+		}
+	}
+	cout<<sum;
+	return 0;
+}
